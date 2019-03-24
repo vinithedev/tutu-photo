9dfef1b6ab1994b7977f4b4df9753dc54205ead4
Vinicius Santos <vinithedev@gmail.com>
2019-03-24 19:49:19 -0300

    Added ScrollView

--- app/src/main/java/vinithedev/tutuphoto/MainActivity.java
+++ app/src/main/java/vinithedev/tutuphoto/MainActivity.java
@@ -1,16 +1,19 @@
 package vinithedev.tutuphoto;
 
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonClean, buttonNext;
+    EditText editTextId, editTextNumber, editTextEquipmentInstalation, editTextAntennaInstalation, editTextConnection, editTextObservation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Spinner's HINT
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerNetwork);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item) {
+        final Spinner spinner = (Spinner) findViewById(R.id.spinnerNetwork);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item) {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
@@ -50,5 +53,37 @@ public class MainActivity extends AppCompatActivity {
         spinner.setSelection(adapter.getCount()); //set the hint the default selection so it appears on launch.
 //        spinner.setOnItemSelectedListener(this);
 
-    }
+        buttonClean = (Button) findViewById(R.id.buttonClean);
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        editTextId = (EditText) findViewById(R.id.editTextId);
+        editTextNumber = (EditText) findViewById(R.id.editTextNumber);
+        editTextEquipmentInstalation = (EditText) findViewById(R.id.editTextEquipmentInstalation);
+        editTextAntennaInstalation = (EditText) findViewById(R.id.editTextAntennaInstalation);
+        editTextConnection = (EditText) findViewById(R.id.editTextConnection);
+        editTextObservation = (EditText) findViewById(R.id.editTextObservation);
+
+        buttonClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextId.setText("");
+                editTextNumber.setText("");
+                editTextEquipmentInstalation.setText("");
+                editTextAntennaInstalation.setText("");
+                editTextConnection.setText("");
+                editTextObservation.setText("");
+                spinner.setSelection(adapter.getCount());
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+    }//End of onCreate
 }

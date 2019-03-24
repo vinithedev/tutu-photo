38f16862691abc5a390b766e6da90edf4d843281
Vinicius Santos <vinithedev@gmail.com>
2019-03-24 15:27:53 -0300

    Finished interface

--- app/src/main/java/vinithedev/tutuphoto/MainActivity.java
+++ app/src/main/java/vinithedev/tutuphoto/MainActivity.java
@@ -1,7 +1,14 @@
 package vinithedev.tutuphoto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Spinner's HINT
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerNetwork);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                View v = super.getView(position, convertView, parent);
+                if (position == getCount()) {
+                    ((TextView)v.findViewById(android.R.id.text1)).setText("");
+                    ((TextView)v.findViewById(android.R.id.text1)).setHint(getItem(getCount())); //"Hint to be displayed"
+                }
+
+                return v;
+            }
+
+            @Override
+            public int getCount() {
+                return super.getCount()-1; // you dont display last item. It is used as hint.
+            }
+
+        };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add("Primary");
+        adapter.add("Secondary");
+        adapter.add("Primary and Secondary");
+        adapter.add("Network"); //HINT
+
+        spinner.setAdapter(adapter);
+        spinner.setSelection(adapter.getCount()); //set the hint the default selection so it appears on launch.
+//        spinner.setOnItemSelectedListener(this);
+
     }
 }
